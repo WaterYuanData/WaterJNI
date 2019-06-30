57bfcf64bd3b6b6ae2b31ea070889c2fb829278b
WaterYuan <water.yuan.data@gmail.com>
2019-06-30 08:50:31 +0800

    导入bzip2，修改了main方法的名字

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include "bs.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_yuan_waterjni_MainActivity_stringFromJNI(
@@ -7,4 +8,54 @@ Java_com_yuan_waterjni_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" {
+int mydiff(int argc, char *argv[]);
+int mypatch(int argc, char *argv[]);
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_yuan_waterjni_MainActivity_diff(JNIEnv *env, jobject instance, jstring oldpath_,
+                                         jstring newpath_, jstring patch_) {
+//    const char *oldpath = env->GetStringUTFChars(oldpath_, 0);
+//    const char *newpath = env->GetStringUTFChars(newpath_, 0);
+//    const char *patch = env->GetStringUTFChars(patch_, 0);
+//    // TODO
+//    env->ReleaseStringUTFChars(oldpath_, oldpath);
+//    env->ReleaseStringUTFChars(newpath_, newpath);
+//    env->ReleaseStringUTFChars(patch_, patch);
+
+    const char *argv[4];
+    argv[0] = "bsdiff";
+    argv[1] = (env)->GetStringUTFChars(oldpath_, 0);
+    argv[2] = (env)->GetStringUTFChars(newpath_, 0);
+    argv[3] = (env)->GetStringUTFChars(patch_, 0);
+    //该函数用于生成差分包
+    mydiff(4, const_cast<char **>(argv));
+    (env)->ReleaseStringUTFChars(oldpath_, argv[1]);
+    (env)->ReleaseStringUTFChars(newpath_, argv[2]);
+    (env)->ReleaseStringUTFChars(patch_, argv[3]);
+    free((void *) argv[0]);
+    free(argv);
+    return 0;
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_yuan_waterjni_MainActivity_patch(JNIEnv *env, jobject instance, jstring oldpath_,
+                                          jstring newpath_, jstring patch_) {
+    const char *argv[4];
+    argv[0] = "bspatch";
+    argv[1] = env->GetStringUTFChars(oldpath_, 0);
+    argv[2] = env->GetStringUTFChars(newpath_, 0);
+    argv[3] = env->GetStringUTFChars(patch_, 0);
+    //该函数用于合并差分包
+    mypatch(4, const_cast<char **>(argv));
+    env->ReleaseStringUTFChars(oldpath_, argv[1]);
+    env->ReleaseStringUTFChars(newpath_, argv[2]);
+    env->ReleaseStringUTFChars(patch_, argv[3]);
+    free(argv);
+    return 0;
 }
--- app/src/main/java/com/yuan/waterjni/MainActivity.java
+++ app/src/main/java/com/yuan/waterjni/MainActivity.java
@@ -1,10 +1,17 @@
 package com.yuan.waterjni;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -15,11 +22,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        Log.i(TAG, "onCreate: " + BuildConfig.VERSION_NAME);
+        final TextView tv = findViewById(R.id.sample_text);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Example of a call to a native method
+                tv.setText(stringFromJNI());
+            }
+        });
+        tv.setText("当前版本：" + BuildConfig.VERSION_NAME);
+        findViewById(R.id.diff).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick: ");
+                long s = System.currentTimeMillis();
+                diff(old, newp, patch);
+                long s1 = System.currentTimeMillis();
+                Toast.makeText(MainActivity.this, "生成差分包成功，用时:" + (s1 - s) + "ms", Toast.LENGTH_SHORT).show();
+            }
+        });
+        findViewById(R.id.patch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick: ");
+                long s2 = System.currentTimeMillis();
+                patch(old, tmp, patch);
+                long s3 = System.currentTimeMillis();
+                Toast.makeText(MainActivity.this, "差分包合并成功，用时:" + (s3 - s2) + "ms", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    //旧版本
+    String old = getsdpath() + "old.apk";
+    //新版本
+    String newp = getsdpath() + "new.apk";
+    //差分包
+    String patch = getsdpath() + "patch.patch";
+    //旧版apk和差分包合并生成的新版apk
+    String tmp = getsdpath() + "new2.apk";
+
+    private String getsdpath() {
+        Log.i(TAG, "getsdpath: " + Environment.getExternalStorageDirectory().getPath());
+        return Environment.getExternalStorageDirectory().getPath() + File.separator;
+    }
+
+    //生成差分包
+    public native int diff(String oldpath, String newpath, String patch);
+
+    //旧apk和差分包合并
+    public native int patch(String oldpath, String newpath, String patch);
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
